4be557aae0be3b7b6f802f6ec2f1ec8d4ec8069e
Luis Hermida <luis.hn@zoho.com>
2020-07-29 06:49:04 -0500

    Added PostgreSQL enviroment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -13,7 +15,9 @@ print("Starting Server...")
 
 app = Flask(__name__)
 app.secret_key = "key-1234567890"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)  # Allow us to add resources to app
 
